4191c6aa240ac6c8f3c5ed813d1dac118c7b756e
Diana <diana.hovhannisyan.0301@mail.ru>
2025-03-28 22:18:52 +0400

    окно регестрации

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -18,81 +18,13 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView storyTextView;
-    private LinearLayout choicesContainer;
-
-    private int currentSceneId = 1;
-    private JSONObject storyData;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        storyTextView = findViewById(R.id.storyTextView);
-        choicesContainer = findViewById(R.id.choicesContainer);
 
-
-        try {
-            storyData = new JSONObject(loadJSONFromAsset());
-            loadScene(currentSceneId);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
-    private String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream is = getAssets().open("story.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return json;
-    }
-
-    private void loadScene(int sceneId) throws JSONException {
-        JSONArray scenes = storyData.getJSONArray("scenes");
-
-        for (int i = 0; i < scenes.length(); i++) {
-            JSONObject scene = scenes.getJSONObject(i);
-
-            if (scene.getInt("id") == sceneId) {
-                String sceneText = scene.getString("text");
-                storyTextView.setText(sceneText);
-
-                if (scene.has("choices")) {
-                    JSONArray choices = scene.getJSONArray("choices");
-                    for (int j = 0; j < choices.length(); j++) {
-                        JSONObject choice = choices.getJSONObject(j);
-                        String choiceText = choice.getString("text");
-
-                        Button choiceButton = new Button(this);
-                        choiceButton.setText(choiceText);
-                        choiceButton.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                try {
-                                    int nextSceneId = choice.getInt("nextScene");
-                                    loadScene(nextSceneId);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-
-                        choicesContainer.addView(choiceButton);
-                    }
-                }
-                break;
-            }
-        }
-    }
 }
