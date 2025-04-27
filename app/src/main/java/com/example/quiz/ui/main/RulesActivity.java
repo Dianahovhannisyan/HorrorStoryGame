933eb8bf57b4532068ad3bd50173bfe142f44851
Diana <diana.hovhannisyan.0301@mail.ru>
2025-04-27 21:20:03 +0400

    StartWindowActivity

--- app/src/main/java/com/example/quiz/ui/main/RulesActivity.java
+++ app/src/main/java/com/example/quiz/ui/main/RulesActivity.java
@@ -1,24 +1,35 @@
 package com.example.quiz.ui.main;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import com.example.quiz.GameActivity;
 import com.example.quiz.R;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RulesActivity extends AppCompatActivity {
 
+    private ImageView swordsIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rules);
 
+        swordsIcon = findViewById(R.id.swordsIcon);
 
+        swordsIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+
+    private void startGame() {
+        Intent intent = new Intent(RulesActivity.this, GameActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
