0750454c2fbf655bea3785e9cec1ed6541d5a4c5
Diana <diana.hovhannisyan.0301@mail.ru>
2025-05-26 09:05:52 +0400

    GameWin

--- app/src/main/java/com/example/quiz/GameWin.java
+++ app/src/main/java/com/example/quiz/GameWin.java
@@ -1,6 +1,9 @@
 package com.example.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.quiz.ui.main.GameOverActivity;
+
 public class GameWin extends AppCompatActivity {
+
+    Button retryButton;
+    TextView quitText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_game_win);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_game_over);
+
+        retryButton = findViewById(R.id.retryButton);
+        quitText = findViewById(R.id.quitText);
+
+        retryButton.setOnClickListener(view -> {
+            Intent intent = new Intent(GameWin.this, GameActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        quitText.setOnClickListener(view -> {
+            finishAffinity();
         });
     }
 }
